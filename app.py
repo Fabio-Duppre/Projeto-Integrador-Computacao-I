967fbc0c9ac05eb7c739de7960f52dca8b602f40
Fabio <fabio.duppre@gmail.com>
2026-04-03 18:00:32 -0300

    adicionando codigo da tela de dashboard, list e realtorio, e ajustando controle de seção

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from db import db
 from models import Resultado, Usuario
 from datetime import datetime
+from sqlalchemy import func, desc
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "chave"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///resultados.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -18,6 +20,62 @@ def formulario():
     age             = request.args.get("age")
     course          = request.args.get("course")
     return render_template("index.html", name=name, age=age, course=course)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastrar.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    if "usuario_id" not in session:
+        return redirect(url_for("login"))
+    
+
+    total_respostas = Resultado.query.count()
+
+    if total_respostas is None:
+        total_respostas = 0
+
+    media_stress = db.session.query(func.avg(Resultado.sScore)).scalar()
+
+    if media_stress is None:
+        media_stress = 0
+
+    
+    curso_repetido = db.session.query(Resultado.course).group_by(Resultado.course).order_by(desc(func.count(Resultado.course))).first()
+    curso_repetido = curso_repetido[0]
+
+    if (curso_repetido == "None") or (curso_repetido == "desconhecido"):
+        curso_repetido = "Não informado"
+
+    return render_template("dashboard.html", total_respostas = total_respostas, media_stress = media_stress, curso_repetido = curso_repetido)
+
+
+@app.route("/lista_aluno")
+def lista_aluno():
+    if "usuario_id" not in session:
+        return redirect(url_for("login"))
+    
+    
+    lista = Resultado.query.all()
+
+    return render_template("lista_aluno.html", lista = lista)
+
+
+@app.route("/relatorio")
+def relatorio():
+    if "usuario_id" not in session:
+        return redirect(url_for("login"))
+
+
+    return render_template("relatorio.html")
 
 
 @app.route("/registrar", methods=['POST'])
@@ -47,21 +105,6 @@ def registrar():
     return redirect(url_for("homepage"))
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@app.route("/cadastrar")
-def cadastrar():
-    return render_template("cadastrar.html")
-
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
-
 @app.route("/cadastrar_usuario", methods=['POST'])
 def cadastrar_usuario():
     name        = request.form['name']
@@ -82,11 +125,20 @@ def cadastrar_usuario():
 
 @app.route("/valida_usuario", methods=['POST'])
 def valida_usuario():
-    if (request.form['email'] == "admin@teste.com") and (request.form['password'] == "admin") :
+    validar = Usuario.query.filter_by(email=request.form['email']).first()    
+
+    if (request.form['email'] == validar.email) and (request.form['password'] == validar.password):
+        session["usuario_id"] = validar.id
         return redirect(url_for("dashboard"))
     
     else:
         return redirect(url_for("cadastrar"))
+
+
+@app.route("/logout")
+def logout():    
+    session.clear()
+    return redirect(url_for("login"))
 
     
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -17,6 +17,6 @@ class Usuario(db.Model):
     __tablename__ = 'usuarios'
 
     id      = db.Column(db.Integer, primary_key=True)
-    nome    = db.Column(db.String(50), nullable=False)
+    name    = db.Column(db.String(50), nullable=False)
     email   = db.Column(db.String(50), nullable= False)
-    senha   = db.Column(db.String(50), nullable=False)
+    password   = db.Column(db.String(50), nullable=False)
